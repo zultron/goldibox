9a4cd9875e049ba7260ea0bc7aa10a8b32589ffa
John Morris <john@zultron.com>
2017-10-05 20:53:05 -0500

    Initial commit

---
+++ mk-incubator-comp.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import hal, time
+
+# Set up component
+h = hal.component("incubator")
+
+# Inputs:
+# - Min/max/current temp settings
+h.newpin("temp-min", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("temp-max", hal.HAL_FLOAT, hal.HAL_IN)
+h.newpin("temp-cur", hal.HAL_FLOAT, hal.HAL_IN)
+# - Enable
+h.newpin("enable", hal.HAL_BIT, hal.HAL_IN)
+
+# Outputs:  
+# - On/off switch
+h.newpin("switch-on", hal.HAL_BIT, hal.HAL_OUT)
+# - Heat/cool selector switch (1/0)
+h.newpin("switch-heat", hal.HAL_BIT, hal.HAL_OUT)
+# - Error
+h.newpin("error", hal.HAL_BIT, hal.HAL_OUT)
+
+# Init values
+h['error'] = h['temp-max'] <= h['temp-min']
+h['switch-on'] = 0
+h['switch-heat'] = 0
+
+# Mark the component as 'ready'
+h.ready()
+
+
+
+while True:
+    time.sleep(0.1)
+
+    # Error condition:  temp-max <= temp-min
+    if h['temp-max'] <= h['temp-min']:
+        h['switch-on'] = 0
+        h['error'] = 1
+        continue
+    else:
+        h['error'] = 0
+
+    # Enable input
+    if not h['enable']:
+        # Disable
+        h['switch-on'] = 0
+        continue
+
+    # Heat/cool selector switch
+    temp_midpoint = (h['temp-min'] + h['temp-max']) / 2.0
+    h['switch-heat'] = h['temp-cur'] > temp_midpoint
+
+    # On/off switch
+    h['switch-on'] = (h['temp-cur'] > h['temp-max']) or \
+                     (h['temp-cur'] < h['temp-min'])
---
+++ setup.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# Copyright 2013
+# Charles Steinkuehler <charles@steinkuehler.net>
+#
+# Modified for the mk-incubator project by John Morris
+# <john@zultron.com> 2017-010; see
+# https://github.com/zultron/mk-incubator
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+dtbo_err () {
+	echo "Error loading device tree overlay file: $DTBO" >&2
+	exit 1
+}
+
+pin_err () {
+	echo "Error exporting pin:$PIN" >&2
+	exit 1
+}
+
+dir_err () {
+	echo "Error setting direction:$DIR on pin:$PIN" >&2
+	exit 1
+}
+
+SLOTS=/sys/devices/bone_capemgr.*/slots
+
+# Make sure required device tree overlay(s) are loaded
+for DTBO in cape-universal cape-bone-iio ; do
+
+	if grep -q $DTBO $SLOTS ; then
+		echo $DTBO overlay found
+	else
+		echo Loading $DTBO overlay
+		sudo -A su -c "echo $DTBO > $SLOTS" || dtbo_err
+		sleep 1
+	fi
+done;
+
+if [ ! -r /sys/devices/ocp.*/helper.*/AIN0 ] ; then
+	echo Analog input files not found in /sys/devices/ocp.*/helper.* >&2
+	exit 1;
+fi
+
+if [ ! -r /sys/class/uio/uio0 ] ; then
+	echo PRU control files not found in /sys/class/uio/uio0 >&2
+	exit 1;
+fi
+
+# Export GPIO pins:
+# One pin needs to be exported to enable the low-level clocks for the GPIO
+# modules (there is probably a better way to do this)
+# 
+# Direct PRU inputs and outputs do not need to be configured here, the pin
+# mux setup (which is handled by the device tree overlay) should be all
+# the setup needed.
+# 
+# Any GPIO pins driven by the hal_bb_gpio driver do not need to be
+# configured here.  The hal_bb_gpio module handles setting the output
+# enable bits properly.  These pins _can_ however be set here without
+# causing problems.  You may wish to do this for documentation or to make
+# sure the pin starts with a known value as soon as possible.
+#
+
+sudo $(which config-pin) -f - <<- EOF
+
+# Outputs
+	P9.12	low	# ENA	Peltier enable
+	P9.14	low	# In1	Peltier +
+	P9.16	low	# In2	Peltier -
+	P9.18	low	# In3	Fan +
+	P9.20	low	# In4	Fan -
+	P9.22	low	# ENB	Fan enable
+
+# Inputs
+#	P9.36		AIN5	Thermistor input
+EOF
+
