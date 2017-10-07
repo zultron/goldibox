be3def54637002f411c1e8fb936f6631438c723c
John Morris <john@zultron.com>
2017-10-06 21:45:52 -0500

    set.sh initial commit

---
+++ set.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+if test "$1" = shutdown; then
+    set -x
+    exec halcmd sets shutdown 1
+fi
+
+min=$1
+max=$2
+hyst=$3
+
+set -x
+halcmd sets temp-min $min
+halcmd sets temp-max $max
+halcmd sets hysteresis $hyst
