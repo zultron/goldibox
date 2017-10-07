a1c619b2b11aaaa9040faeebe54b86e6c34ccf7c
John Morris <john@zultron.com>
2017-10-06 21:45:30 -0500

    better status messages; hysteresis; logic changes
    - Clean up status messages
    - Display messages on state change
    - Clean up logic to make better messaging possible
    - Clean up logic to make hysteresis possible
    - Only sample once per cycle to avoid any funny race conditions
    - Add hysteresis

--- mk-incubator-comp.py
+++ mk-incubator-comp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import hal, time, sys
+import hal, time, sys, datetime
 
 # Set up component
 h = hal.component("incubator")
@@ -10,6 +10,8 @@ h = hal.component("incubator")
 h.newpin("temp-min", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("temp-max", hal.HAL_FLOAT, hal.HAL_IN)
 h.newpin("temp-cur", hal.HAL_FLOAT, hal.HAL_IN)
+# - Hysteresis
+h.newpin("hysteresis", hal.HAL_FLOAT, hal.HAL_IN)
 # - Enable
 h.newpin("enable", hal.HAL_BIT, hal.HAL_IN)
 # - Exit
@@ -24,48 +26,77 @@ h.newpin("switch-heat", hal.HAL_BIT, hal.HAL_OUT)
 h.newpin("error", hal.HAL_BIT, hal.HAL_OUT)
 
 # Init values
-h['error'] = h['temp-max'] <= h['temp-min']
+h['error'] = (h['temp-max'] <= h['temp-min']) or (h['hysteresis'] < 0)
 h['switch-on'] = 0
 h['switch-heat'] = 0
 
+def infomsg(msg):
+    sys.stderr.write("%s Incubator component:  %s\n" %
+                     (str(datetime.datetime.now()), msg))
+
 # Mark the component as 'ready'
-sys.stderr.write("Incubator component:  initialized\n")
+infomsg("Initialized")
 h.ready()
-
-
 
 while True:
     time.sleep(0.1)
 
+    # Take one sample for consistency
+    enable = h['enable']
+    shutdown = h['shutdown']
+    temp_cur = h['temp-cur']
+    temp_max = h['temp-max']
+    temp_min = h['temp-min']
+    hysteresis = h['hysteresis']
+    
     # Exit
-    if h['shutdown']:
-        sys.stderr.write(
-            "Incubator component:  got shutdown signal = %s\n" % h['shutdown'])
+    if shutdown:
+        infomsg("Got shutdown signal = %s" % shutdown)
         break
 
     # Error condition:  temp-max <= temp-min
-    if h['temp-max'] <= h['temp-min']:
+    if (temp_max <= temp_min) or (hysteresis < 0):
+        if not h['error']:
+            if temp_max <= temp_min:
+                infomsg("ERROR:  temp_max <= temp_min")
+            if hysteresis < 0:
+                infomsg("ERROR:  hysteresis < 0")
         h['switch-on'] = 0
         h['error'] = 1
         continue
     else:
+        if h['error']:
+            infomsg("Recovered from error conditions")
         h['error'] = 0
 
     # Enable input
-    if not h['enable']:
+    if not enable:
         # Disable
         h['switch-on'] = 0
         continue
 
-    # Heat/cool selector switch
-    temp_midpoint = (h['temp-min'] + h['temp-max']) / 2.0
-    h['switch-heat'] = h['temp-cur'] < temp_midpoint
 
-    # On/off switch
-    h['switch-on'] = (h['temp-cur'] > h['temp-max']) or \
-                     (h['temp-cur'] < h['temp-min'])
+    # On/off and heat/cool switches
+    if ((temp_cur - hysteresis) > temp_max):
+        if not h['switch-on']:
+            infomsg("Turning on heat; cur temp %.1f (hyst %.1f)" %
+                    (temp_cur, hysteresis))
+        h['switch-heat'] = 1
+        h['switch-on'] = 1
+    elif ((temp_cur + hysteresis) < temp_min):
+        if not h['switch-on']:
+            infomsg("Turning on cool; cur temp %.1f (hyst %.1f)" %
+                    (temp_cur, hysteresis))
+        h['switch-heat'] = 0
+        h['switch-on'] = 1
+    elif ((temp_cur > temp_min) and (temp_cur < temp_max)):
+        if h['switch-on']:
+            infomsg("Turning off; cur temp %.1f" % temp_cur)
+        h['switch-on'] = 0
 
-sys.stderr.write("Incubator component:  exiting\n")
+
+
+infomsg("Exiting")
 
 # Shut things off
 h['switch-on'] = 0
