2967e910af5081ca0b0fb2b26ec345b44b66e1a8
John Morris <john@zultron.com>
2017-10-20 03:36:55 -0500

    incubator-bb.hal and setup.sh:  redo pins for PocketBeagle

--- setup.sh
+++ setup.sh
@@ -1,94 +1,24 @@
 #!/bin/bash
-# Copyright 2013
-# Charles Steinkuehler <charles@steinkuehler.net>
-#
-# Modified for the mk-incubator project by John Morris
-# <john@zultron.com> 2017-010; see
-# https://github.com/zultron/mk-incubator
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-dtbo_err () {
-	echo "Error loading device tree overlay file: $DTBO" >&2
-	exit 1
-}
-
-pin_err () {
-	echo "Error exporting pin:$PIN" >&2
-	exit 1
-}
-
-dir_err () {
-	echo "Error setting direction:$DIR on pin:$PIN" >&2
-	exit 1
-}
-
-SLOTS=/sys/devices/bone_capemgr.*/slots
-
-# Make sure required device tree overlay(s) are loaded
-for DTBO in cape-universal cape-bone-iio ; do
-
-	if grep -q $DTBO $SLOTS ; then
-		echo $DTBO overlay found
-	else
-		echo Loading $DTBO overlay
-		sudo -A su -c "echo $DTBO > $SLOTS" || dtbo_err
-		sleep 1
-	fi
-done;
-
-if [ ! -r /sys/devices/ocp.*/helper.*/AIN0 ] ; then
-	echo Analog input files not found in /sys/devices/ocp.*/helper.* >&2
-	exit 1;
-fi
-
-if [ ! -r /sys/class/uio/uio0 ] ; then
-	echo PRU control files not found in /sys/class/uio/uio0 >&2
-	exit 1;
-fi
-
-# Export GPIO pins:
-# One pin needs to be exported to enable the low-level clocks for the GPIO
-# modules (there is probably a better way to do this)
-# 
-# Direct PRU inputs and outputs do not need to be configured here, the pin
-# mux setup (which is handled by the device tree overlay) should be all
-# the setup needed.
-# 
-# Any GPIO pins driven by the hal_bb_gpio driver do not need to be
-# configured here.  The hal_bb_gpio module handles setting the output
-# enable bits properly.  These pins _can_ however be set here without
-# causing problems.  You may wish to do this for documentation or to make
-# sure the pin starts with a known value as soon as possible.
-#
-# Pinouts:
-# http://beagleboard.org/support/bone101
+# PocketBeagle pinouts:
+# https://github.com/beagleboard/pocketbeagle/wiki/FAQ
 
 sudo $(which config-pin) -f - <<- EOF
 
-# Outputs
-	P8.08	low	# ENA	Peltier enable
-	P8.10	low	# In1	Peltier +
-	P8.12	low	# In2	Peltier -
-	P8.14	low	# In3	Fan -
-	P8.16	low	# In4	Fan +
-	P8.18	low	# ENB	Fan enable
+# PocketBeagle
+# - Outputs
+	P1.02	low	# In2/In3	Peltier -/hot
+	P1.04	low	# In1/In4	Peltier +/cool
+	P1.06	low	# ENA/ENB	Peltier enable
+	P1.20	low	# MOSFET S	Fan on
 
-# Inputs
-#	P9.36		AIN5	Thermistor input
+# - Inputs
+#	P1.17		VREFN	AIN/thermistor ground
+#	P1.18		VREFP	AIN/thermistor 1.8V REF+
+#	P1.19		AIN0	Thermistor input 0
+#	P1.21		AIN1	Thermistor input 1
+#	P1.23		AIN2	Thermistor input 2
+#
+#	P1.01		VIN-AC	Power input
+#	P1.22		GND	Ground
 EOF
-
-# The above may fail if run twice; don't make halrun fail
-exit 0
