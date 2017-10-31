a0005fbcccdb7f3d0eec347b478743418761c60c
John Morris <john@zultron.com>
2017-10-31 00:59:54 -0500

    Port some minor things from remote branch

--- docker/qqvcp.sh
+++ docker/qqvcp.sh
@@ -10,7 +10,6 @@ usage() {
 	test -z "$ERRMSG" || echo "ERROR:  $ERRMSG"
 	echo "Usage:"
 	echo "    $0 build             Build the Docker container image"
-	echo "    $0 anddemo-build     Build the AND demo"
 	echo "    $0                   Run interactive shell in container"
 	echo "    $0 CMD [ ARGS ... ]  Run CMD in container"
     } >&2
@@ -45,40 +44,6 @@ if test "$1" = build; then
     cd "$TOPDIR/docker"
     set -x
     exec docker build -t $IMAGE "$@" .
-fi
-
-if test "$1" = anddemo-build; then
-    run bash -xec "
-	mkdir -p $TOPDIR/node
-	cd $TOPDIR/node
-	# - AND demo:  clone
-	test -d anddemo || \
-	    git clone https://github.com/machinekoder/anddemo.git
-	# - qmlweb:  clone and install
-	test -d qmlweb || \
-	    git clone -b webvcp https://github.com/machinekoder/qmlweb.git
-	( cd qmlweb && npm install && npm run build )
-	# - webvcp:  clone and install
-	test -d webvcp || \
-	    git clone -b pr-svc-disc https://github.com/zultron/webvcp.git
-	    # FIXME https://github.com/qtquickvcp/webvcp/pull/1
-	    #git clone https://github.com/machinekoder/webvcp.git
-	( cd webvcp && npm install )
-    "
-fi
-
-if test "$1" = anddemo; then
-    run bash -xec "
-	# - Start system services
-	for s in rsyslog dbus avahi-daemon; do sudo /etc/init.d/\$s start; done
-	# - Start AND demo
-	( cd node/anddemo && exec ./run.py ) &
-	sleep 2
-	# - Start web service
-	( cd node/webvcp && exec node index.js ) &
-	sleep 2
-	exec bash -i
-    "
 fi
 
 # Check for existing containers
